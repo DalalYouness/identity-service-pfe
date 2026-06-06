7e2bf4dc09f052ebd3557fd2cc51e854c3d338d1
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-06-06 19:30:09 +0100

    first commit

---
+++ src/main/java/com/dalal/identityservicepfe/IdentityServicePfeApplication.java
@@ -0,0 +1,13 @@
+package com.dalal.identityservicepfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityServicePfeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityServicePfeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dalal/identityservicepfe/IdentityServicePfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalal.identityservicepfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServicePfeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
