22682cb926d458d2a6ac2227620e2c46b85b4f7d
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-06-08 00:19:57 +0100

    main config

--- src/main/resources/db/migration/V1__init_auth_schema.sql
+++ src/main/resources/db/migration/V1__init_auth_schema.sql
@@ -5,34 +5,35 @@ CREATE TABLE roles (
 
 CREATE TABLE users (
         id BIGINT AUTO_INCREMENT PRIMARY KEY,
-        email VARCHAR(180) NOT NULL UNIQUE,
+        email VARCHAR(100) NOT NULL UNIQUE,
         password VARCHAR(255) NULL,
         username VARCHAR(100),
-        auth_provider VARCHAR(50) DEFAULT 'LOCAL',
+        auth_provider VARCHAR(30) DEFAULT 'LOCAL',
         account_status VARCHAR(20) DEFAULT 'ACTIVE',
         created_at DATETIME,
         updated_at DATETIME
-) ENGINE=Inn
-CREATE TABLE profils (
-        id BIGINT PRIMARY KEY, -- هو بـيـدو PK و FK
-        type_profil VARCHAR(30) NOT NULL, -- Discriminator: ADMIN, CLIENT, PRESTATAIRE
-        firstname VARCHAR(100),
-        lastname VARCHAR(100),
-        phone_number VARCHAR(20),
-        birthdate DATE,
-        gender VARCHAR(10),
-        photo_url VARCHAR(255),
-        address VARCHAR(255),
-        city VARCHAR(100),
-        country VARCHAR(100),
-        bio TEXT,
-        is_available BOOLEAN DEFAULT TRUE,
-        intervention_area VARCHAR(255), -- خاص فقط بالـ PRESTATAIRE
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        CONSTRAINT fk_profil_user FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
+CREATE TABLE profils (
+        id BIGINT PRIMARY KEY,
+        type_profil VARCHAR(20) NOT NULL, -- Discriminator: ADMIN, CLIENT, PRESTATAIRE
+        firstname VARCHAR(50) NOT NULL,
+        lastname VARCHAR(50) NOT NULL,
+        phone_number VARCHAR(20) NOT NULL UNIQUE,
+        birthdate DATE NOT NULL,
+        gender VARCHAR(10) NOT NULL,
+        photo_url VARCHAR(255),
+        address VARCHAR(255) NOT NULL,
+        city VARCHAR(100) NOT NULL,
+        country VARCHAR(100) NOT NULL,
+        bio TEXT,
+        is_available BOOLEAN DEFAULT TRUE,
+        intervention_area VARCHAR(255), -- FOR PRESTATAIRE
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        CONSTRAINT fk_profil_user FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE,
+        CONSTRAINT chk_birthdate CHECK (birthdate <= '2008-12-31')
+) ENGINE=InnoDB;
 
 CREATE TABLE user_roles (
         user_id BIGINT NOT NULL,
